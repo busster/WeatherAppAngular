e7b3adaf966c80c72478785d4bab3db1c97bf1fa
busster <jasonmbuss@gmail.com>
2017-03-04 21:42:39 -0600

    saf

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
-const path = require('path');
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 
 // Run the app by serving the static files
@@ -34,3 +28,10 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+const path = require('path');
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
